73af5ba6d8a6ecdc1610f70bafdd6bc8c4ef6929
bryantgillespie <bryant@hireclockwork.com>
2022-10-11 23:52:12 -0400

    [fix] nuxtapp instance unavailable issue with useCookie()

--- plugins/directus/index.ts
+++ plugins/directus/index.ts
@@ -1,32 +1,35 @@
 import { BaseStorage, Directus } from '@directus/sdk'
 import { useAuth } from '~~/store/auth'
+import { readRawCookies, writeClientCookie, writeServerCookie } from './cookies'
 
 // Make sure you review the Directus SDK documentation for more information
 // https://docs.directus.io/reference/sdk.html
-
 export default defineNuxtPlugin(async (nuxtApp) => {
   const { directusUrl } = useRuntimeConfig()
-
   const auth = useAuth()
 
   // Create a new storage class to use with the SDK
   // Needed for the SSR to play nice with the SDK
   class CookieStorage extends BaseStorage {
-    deletedKeys = new Set<string>()
     get(key) {
-      if (this.deletedKeys.has(key)) return null
-      const cookie = useCookie(key)
-      return cookie.value
+      const cookies = readRawCookies(
+        process.server ? nuxtApp.ssrContext.req : null
+      )
+      return cookies[key]
     }
     set(key, value) {
-      this.deletedKeys.delete(key)
-      const cookie = useCookie(key)
-      return (cookie.value = value)
+      if (process.client) {
+        writeClientCookie(key, value)
+      } else if (process.server) {
+        writeServerCookie(nuxtApp.ssrContext.res, key, value)
+      }
     }
     delete(key) {
-      this.deletedKeys.add(key)
-      const cookie = useCookie(key)
-      return (cookie.value = null)
+      if (process.client) {
+        writeClientCookie(key, null)
+      } else if (process.server) {
+        writeServerCookie(nuxtApp.ssrContext.res, key, null)
+      }
     }
   }
 
@@ -38,13 +41,13 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     },
   })
 
-  // Inject the SDK into the Nuxt app
+  //   // Inject the SDK into the Nuxt app
   nuxtApp.provide('directus', directus)
 
-  const token = await directus.auth.token
   const side = process.server ? 'server' : 'client'
+  const token = await directus.auth.token
 
-  // If there's a token but we don't have a user, fetch the user
+  //   If there's a token but we don't have a user, fetch the user
   if (!auth.isLoggedIn && token) {
     console.log('Token found, fetching user from ' + side)
     console.log('Token is', token)
