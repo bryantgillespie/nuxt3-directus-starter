3ff59359f46ab2f81f6284a3787cc6a66385d24c
bryantgillespie <bryant@hireclockwork.com>
2022-12-12 21:18:04 -0500

    [fix] add NuxtLoading Indicator

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <NuxtLayout>
+      <NuxtLoadingIndicator
+        color="repeating-linear-gradient(to right,#F21BB9
+    0%,#6366f1 100%)"
+      />
       <NuxtPage />
     </NuxtLayout>
   </div>
