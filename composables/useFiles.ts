f9bfe8e204378afedd539024eca18d40bbba6e00
bryantgillespie <hey@bryantgillespie.com>
2023-05-30 08:48:37 -0400

    fix runtimeconfig

--- composables/useFiles.ts
+++ composables/useFiles.ts
@@ -2,7 +2,7 @@ export default function useFiles() {
   const { $config } = useNuxtApp()
 
   function fileUrl(fileId) {
-    return `${$config.directusUrl}/assets/${fileId}`
+    return `${$config.public.directusUrl}/assets/${fileId}`
   }
 
   return {
