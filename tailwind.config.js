e8a4881d83f70063f5040beb479939aa14442d66
bryantgillespie <hey@bryantgillespie.com>
2023-05-30 08:49:20 -0400

    line clamp now built in

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 }
